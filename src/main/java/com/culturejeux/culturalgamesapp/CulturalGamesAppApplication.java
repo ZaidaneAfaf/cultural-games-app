657db55b3af7825da32b94acb6b46e83183dba82
Afaf z <afafz@gmail.com>
2025-11-06 23:17:49 +0100

    Initial project setup: Spring Boot + React + dependencies

---
+++ src/main/java/com/culturejeux/culturalgamesapp/CulturalGamesAppApplication.java
@@ -0,0 +1,13 @@
+package com.culturejeux.culturalgamesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalGamesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulturalGamesAppApplication.class, args);
+	}
+
+}
